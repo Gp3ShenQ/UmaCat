a559869669f57aca82d63e06ce4ec2d03bbd215d
Gp3ShenQ <gp3g4xup6@gmail.com>
2023-10-09 01:00:49 +0800

    v1.3

--- index.js
+++ index.js
@@ -16,3 +16,46 @@ if ($(window).scrollTop() >= 500 ){
 $(".goback").on("click",function(){
     $('html,body').animate({scrollTop:0}, 100)
 })
+
+
+$(".box-wrap1").on("click",function(){
+    catBox(".uma")
+})
+$(".box-wrap2").on("click",function(){
+    catBox(".small")
+})
+$(".box-wrap3").on("click",function(){
+    catBox(".start")
+})
+
+function catBox(name){
+    $(name).css("display" , "flex") 
+    $(".aboutMask").css("display" , "block")
+    $(".aboutCat").css("display" , "block")
+    $(".bgMask").css("display" , "block")
+}
+
+$(".close1").on("click",function(){
+    clear(".uma")
+})
+$(".close2").on("click",function(){
+    clear(".small")
+})
+$(".close3").on("click",function(){
+    clear(".start")
+})
+$(".bgMask").on("click",function(){
+    $(".uma , .small , .start").css("display" , "none")
+    clearBox()
+})
+
+function clear(name){
+    $(name).css("display" , "none")
+    clearBox()
+}
+function clearBox(){
+    $(".aboutMask").css("display" , "none")
+    $(".aboutCat").css("display" , "none")
+    $(".bgMask").css("display" , "none")
+    
+}
